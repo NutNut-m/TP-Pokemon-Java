0cec6fda7791a5296ecd9ae6e955d06acd3e7aa4
P0uetteP0uette <nathan.lyonnet@gmail.com>
2025-12-18 14:29:49 +0100

    initial commit

---
+++ src/main/java/pokemon/Monster.java
@@ -0,0 +1,10 @@
+package pokemon;
+
+public class Monster {
+    protected String name;
+    protected int pv;
+    protected int attack;
+    protected int defense;
+    protected int speed;
+    protected CurrentState state;
+}
